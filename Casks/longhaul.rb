f54d0e60531b264ccc3d3648a28f677c6d435766
Kian Konrad Tajbakhsh <64419110+Kian-hdr@users.noreply.github.com>
2026-09-23 21:46:15 +0200

    Use versioned Longhaul cask URL

--- Casks/longhaul.rb
+++ Casks/longhaul.rb
@@ -2,7 +2,7 @@ cask "longhaul" do
   version "1.0.0"
   sha256 "9816ecb8942b0ec28a22e83cfcdee6f55d38a55cfd10bed448bc80f0d4a109a2"
 
-  url "https://github.com/Kian-hdr/longhaul/releases/download/v1.0.0/Longhaul-1.0.0-arm64.dmg"
+  url "https://github.com/Kian-hdr/longhaul/releases/download/v#{version}/Longhaul-#{version}-arm64.dmg"
   name "Longhaul"
   desc "Keep supported jobs awake and retain recovery checkpoints"
   homepage "https://github.com/Kian-hdr/longhaul"
